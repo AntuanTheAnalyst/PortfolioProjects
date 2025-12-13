f0b614b0612633b200f18206bb392d351e11ea48
Antuan The Analyst <antuan_1148@hotmail.com>
2025-12-13 18:28:45 +1300

    Initialize Flask app with blog routes

---
+++ Blog Website Project/main.py
@@ -0,0 +1,22 @@
+import requests
+from flask import Flask, render_template
+from post import Post
+
+app = Flask(__name__)
+post = Post()
+
+
+@app.route('/')
+def home():
+    return render_template("index_blog_templating.html", posts=post.get_all_posts())
+
+
+@app.route('/post/<int:blog_id>')
+def read_post(blog_id):
+    return render_template("post_blog_templating.html", posts=post.get_all_posts(), blog_id=blog_id)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
