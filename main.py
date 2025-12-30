725efe3838c2ab997906d49673eeb992f8e05aeb
antuan1148 <antuan_1148@hotmail.com>
2025-12-31 08:49:48 +1300

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+import requests
+import smtplib
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+app = Flask(__name__)
+
+MY_EMAIL = "alikes8060@gmail.com"
+MY_PASSWORD = "soupiewhdpkquslm"
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == 'POST':
+        data = request.form
+        name = data["name"]
+        email = data["email"]
+        phone = data["phone"]
+        message = data["message"]
+        content = f"Name: {name}\nEmail: {email}\nPhone Number: {phone}\nMessage: {message}"
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs="aliat8060@gmail.com",
+                msg=f"Subject:New Message\n\n{content}"
+            )
+
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
